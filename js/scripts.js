4ed672c383c29d2127cfb6812c403ff88aa5b064
Connor McCarthy and Vera Weikel <connor@>
2018-08-07 13:58:08 -0700

    add javascript to show flash card answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 $(document).ready (function(){
-  $(".javascript").click(function(){
-  $("p.javascript").toggle();
-  });
-  $(".operators").click(function(){
-    $("p.operators").toggle();
+  // $(".javascript").click(function(){
+  // $("p.javascript").toggle();
+  // });
+  // $(".operators").click(function(){
+  //   $("p.operators").toggle();
+  $("h2").click(function(){
+    $(this).next("p").fadeToggle();
+  // });
   });
 });
