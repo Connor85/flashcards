f04a07293ab561ffd041b33bdc193f6396c398fa
Connor McCarthy and Vera Weikel <vera@>
2018-08-07 13:18:50 -0700

    It will add an update to the files in order to display definitions when toggled.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready (function(){
+  $(".javascript").click(function(){
+  $("p.javascript").toggle();
+  });
+  $(".operators").click(function(){
+    $("p.operators").toggle();
+  });
+});
